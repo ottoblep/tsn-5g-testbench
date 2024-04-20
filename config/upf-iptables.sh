69a1126e77746718461db5b148b57f242030864e
Severin Lochschmidt <seviron53@gmail.com>
2024-04-20 20:38:45 +0200

    Route all UE traffic to PTP Master

--- config/upf-iptables.sh
+++ config/upf-iptables.sh
@@ -2,6 +2,7 @@
 #
 # Configure iptables in UPF
 #
-iptables -t nat -A POSTROUTING -o eth0  -j MASQUERADE
+# This forwards all traffic from the UE to the eth1 interface
+iptables -t nat -A POSTROUTING -o eth1  -j MASQUERADE
 iptables -I FORWARD 1 -j ACCEPT
 
