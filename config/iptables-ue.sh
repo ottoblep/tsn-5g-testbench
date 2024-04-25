3eb41ec214bcc50b6635b93231ebc61b88b4c242
Severin Lochschmidt <seviron53@gmail.com>
2024-04-25 12:57:05 +0200

    Add comments on routing

--- config/iptables-ue.sh
+++ config/iptables-ue.sh
@@ -9,5 +9,8 @@ iptables-nft -I FORWARD -j ACCEPT
 iptables-nft -t nat -I POSTROUTING -o oaitun_ue1 -j MASQUERADE
 iptables-nft -t nat -I POSTROUTING -o eth1 -j MASQUERADE
 
+# Since we can only use the UE as a destination for the tunnel we port forward traffic from the UE to the PTP-Slave
+iptables-nft -t nat -A PREROUTING -p tcp --dport 1111 -j DNAT --to-destination 10.100.202.200:1111
+
 # Allow return traffic
 iptables-nft -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
