d23ae329e2f6ecaa8d52119d96e8a3f1cfba49f0
Severin Lochschmidt <seviron53@gmail.com>
2024-04-25 13:00:43 +0200

    Add comment on veth id script

--- scripts/list_veth_ids.sh
+++ scripts/list_veth_ids.sh
@@ -1,3 +1,6 @@
+# Each container network interface is accessible from the host for monitoring.
+# The interface names on the host are structured as vethXXXXXXX
+# This script identifies the host veth ID of the interface eth0 for each docker container
 for container in $(docker ps -q); do
     iflink=`docker exec -it $container bash -c 'cat /sys/class/net/eth0/iflink'`
     iflink=`echo $iflink|tr -d '\r'`
