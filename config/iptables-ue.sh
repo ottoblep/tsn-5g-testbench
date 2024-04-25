ff0fd863c5650a2606aae35b6a018b1a7a6541c4
Severin Lochschmidt <seviron53@gmail.com>
2024-04-25 12:52:22 +0200

    Disabling path filtering is unnecessary

--- config/iptables-ue.sh
+++ config/iptables-ue.sh
@@ -11,8 +11,3 @@ iptables-nft -t nat -I POSTROUTING -o eth1 -j MASQUERADE
 
 # Allow return traffic
 iptables-nft -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# Disable reverse path filtering
-for i in /proc/sys/net/ipv4/conf/*/rp_filter ; do
-    echo 0 > $i 
-done
