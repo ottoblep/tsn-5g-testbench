b7bc9498c8f07b695b8def3ecbde2c86007fef21
Severin Lochschmidt <seviron53@gmail.com>
2024-04-29 18:15:41 +0200

    Import all forks as submodules
    This helps with organizing them and avoids having to replace links when the sources change.

--- scripts/build_free5gc_base_images.sh
+++ scripts/build_free5gc_base_images.sh
@@ -3,8 +3,9 @@ SCRIPTPATH=`dirname $0`
 cd $SCRIPTPATH/../free5gc-compose
 
 # Patches for free5gc-compose
-# - Clone the free5gc source instead of using a local copy
+# - Use submodule as source 
 git apply ../patches/free5gc-compose/*.patch
+cp -rf ../go-upf ./base # Docker copy only accepts files below it 
 
-docker build -t free5gc/base:latest ./base
+docker build -t free5gc/base:latest -f ./base
 docker build --build-arg F5GC_MODULE=upf -t free5gc/upf-base:latest -f ./base/Dockerfile.nf ./base
