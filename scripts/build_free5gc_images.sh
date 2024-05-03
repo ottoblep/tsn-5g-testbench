d0b7f001e82e95aca58c8ee13e43afb8b5184678
Severin Lochschmidt <seviron53@gmail.com>
2024-05-03 12:35:06 +0200

    Avoid copying upf source

--- scripts/build_free5gc_images.sh
+++ scripts/build_free5gc_images.sh
@@ -1,11 +1,11 @@
 SCRIPTPATH=`dirname $0`
 
-cd $SCRIPTPATH/../free5gc-compose
+# We build with the project root as context to have access to the submodules
+cd $SCRIPTPATH/..
 
 # Patches for free5gc-compose
 # - Use submodule as source 
-git apply ../patches/free5gc-compose/*.patch
-cp -rf ../go-upf ./base # Docker copy only accepts files below it 
+git apply ./patches/free5gc-compose/*.patch --directory free5gc-compose
 
-docker build -t free5gc/base:latest ./base
-docker build --build-arg F5GC_MODULE=upf -t free5gc/upf-base:latest -f ./base/Dockerfile.nf ./base
+docker build -t free5gc/base:latest -f ./free5gc-compose/base .
+docker build --build-arg F5GC_MODULE=upf -t free5gc/upf-base:latest -f ./free5gc-compose/base/Dockerfile.nf .
