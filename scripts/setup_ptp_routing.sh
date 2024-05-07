5710ce78062e7a0ab78979331099753c600a764b
Severin Lochschmidt <seviron53@gmail.com>
2024-05-07 14:13:51 +0200

    Remove unused routing

--- scripts/setup_ptp_routing.sh
+++ scripts/setup_ptp_routing.sh
@@ -1,25 +1,7 @@
-# Setup route from ptp-master to ptp-slave via the 5gs
+# Setup routing for PTP bridge via the 5gs
 # For network graph see docs/structure.drawio
 # This can only run after the data session has been established by the 5gs (oaitun_ue1 created)
 
-# PTP Containers are connected to UE and UPF
-docker exec ptp-slave ip r add 10.100.201.0/24 via 10.100.202.100
-docker exec ptp-master ip r add 10.60.0.0/24 via 10.100.201.100
-
-# Forward everything
-docker exec upf iptables -I FORWARD -j ACCEPT
-docker exec oai-nr-ue iptables-nft -I FORWARD -j ACCEPT
-
-# Allow return traffic
-docker exec upf iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-docker exec oai-nr-ue iptables-nft -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# UE shall use the tunnel for upstream traffic
-docker exec oai-nr-ue ip r add 10.100.201.0/24 dev oaitun_ue1
-
-# We would like to do the same on the UPF side.
-# However, the tunnel only accepts one destination IP (10.60.0.1) in the downstream direction
-# docker exec upf ip r add 10.100.202.0/24 dev upfgtp # This doesnt work
-# Instead we use the UE ip as the destination and port forward to the connected services
-docker exec oai-nr-ue iptables-nft -t nat -I PREROUTING -p tcp --dport 319 -j DNAT --to-destination 10.100.202.200:319
-docker exec oai-nr-ue iptables-nft -t nat -I PREROUTING -p udp --dport 319 -j DNAT --to-destination 10.100.202.200:319
+# UPF and UE use tunnel interfaces to communicate
+docker exec oai-nr-ue ip r add 10.100.200.137 dev oaitun_ue1
+docker exec upf ip r add 10.60.0.1 dev upfgtp
