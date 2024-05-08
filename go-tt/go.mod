2800fcd5e7069dae6e1cd5d42a01d92096737e55
Severin Lochschmidt <seviron53@gmail.com>
2024-05-08 18:27:12 +0200

    Add ds-tt to UE

--- go-tt/go.mod
+++ go-tt/go.mod
@@ -4,7 +4,7 @@ go 1.21
 
 require (
 	github.com/facebook/time v0.0.0-20240501094127-b56da860b6c1
-	golang.org/x/net v0.23.0
+	golang.org/x/net v0.25.0
 )
 
-require golang.org/x/sys v0.18.0 // indirect
+require golang.org/x/sys v0.20.0 // indirect
--- scripts/build_oai_images.sh
+++ scripts/build_oai_images.sh
@@ -4,4 +4,8 @@ cd $SCRIPTPATH/../openairinterface5g
 
 docker build --target ran-base --tag ran-base:latest --file docker/Dockerfile.base.ubuntu20 .
 docker build --target ran-build --tag ran-build:latest --file docker/Dockerfile.build.ubuntu20 .
-docker build --target oai-nr-ue --tag oai-nr-ue:develop --file docker/Dockerfile.nrUE.ubuntu20 .
+
+# Dockerfiles can only copy files below them in the file tree. As such we call docker build from the repo source to be able to include go-tt
+cd $SCRIPTPATH/..
+
+docker build --target oai-nr-ue --tag oai-nr-ue:develop --file openairinterface5g/docker/Dockerfile.nrUE.ubuntu20 .
