629d0f620bded84f53394776f666c205474ca65e
Severin Lochschmidt <seviron53@gmail.com>
2024-05-13 14:40:37 +0200

    Split DS-TT compilation for faster builds

--- scripts/build_oai_images.sh
+++ scripts/build_oai_images.sh
@@ -4,8 +4,9 @@ cd $SCRIPTPATH/../openairinterface5g
 
 docker build --target ran-base --tag ran-base:latest --file docker/Dockerfile.base.ubuntu20 .
 docker build --target ran-build --tag ran-build:latest --file docker/Dockerfile.build.ubuntu20 .
+docker build --target oai-nr-ue --tag oai-nr-ue:latest --file docker/Dockerfile.nrUE.ubuntu20 .
 
 # Dockerfiles can only copy files below them in the file tree. As such we call docker build from the repo source to be able to include go-tt
 cd $SCRIPTPATH/..
 
-docker build --target oai-nr-ue --tag oai-nr-ue:develop --file openairinterface5g/docker/Dockerfile.nrUE.ubuntu20 .
+docker build --target oai-nr-ue-dstt --tag oai-nr-ue-dstt:latest --file openairinterface5g/docker/Dockerfile.nrUEdsTT.ubuntu20 .
