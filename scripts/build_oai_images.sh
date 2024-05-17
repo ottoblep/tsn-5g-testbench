aad64f56d2f72a03e3d0c81b43ecd7e34dbf9afd
Severin Lochschmidt <seviron53@gmail.com>
2024-05-17 15:17:09 +0200

    Fix script path

--- scripts/build_oai_images.sh
+++ scripts/build_oai_images.sh
@@ -6,5 +6,5 @@ docker build --target ran-base --tag ran-base:latest --file docker/Dockerfile.ba
 docker build --target ran-build --tag ran-build:latest --file docker/Dockerfile.build.ubuntu20 .
 docker build --target oai-nr-ue --tag oai-nr-ue:latest --file docker/Dockerfile.nrUE.ubuntu20 .
 
-cd $SCRIPTPATH/..
+cd ..
 docker build --target oai-nr-ue-dstt --tag oai-nr-ue-dstt:latest --file openairinterface5g/docker/Dockerfile.nrUEdsTT.ubuntu20 .
