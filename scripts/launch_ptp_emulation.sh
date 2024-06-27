495ba4f8cf1bf252d7ad3514450d74c4e10d0e33
Severin Lochschmidt <seviron53@gmail.com>
2024-06-27 11:43:53 +0200

    Merge scripts

--- scripts/launch_ptp_emulation.sh
+++ scripts/launch_ptp_emulation.sh
@@ -8,4 +8,9 @@ echo "Launching ptp containers" &&
 docker compose --profile ptpsim up -d &&
 
 echo "Setting up routing for PTP packets" &&
-$(dirname "$0")/setup_ptp_routing.sh
+# For network graph see docs/structure.drawio
+# This can only run after the data session has been established by the 5gs (oaitun_ue1 created)
+
+# UPF and UE use tunnel interfaces to communicate
+docker exec oai-nr-ue ip r add 10.100.200.137 dev oaitun_ue1
+docker exec upf ip r add 10.60.0.1 dev upfgtp
