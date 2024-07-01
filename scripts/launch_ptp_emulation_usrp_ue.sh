2e0f79c9b1d7ca006d5d1e6082d6b964b7b2b6bf
Severin Lochschmidt <seviron53@gmail.com>
2024-07-01 14:46:28 +0200

    Add ptpd as workaround for missing RRC time sync

--- scripts/launch_ptp_emulation_usrp_ue.sh
+++ scripts/launch_ptp_emulation_usrp_ue.sh
@@ -1,7 +1,13 @@
-echo "Launching software ptp client" &&
-docker compose up -d ptp-client &&
+SCRIPTPATH=`dirname $0`
 
-echo "Setting up routing for PTP packets" &&
+echo "Initializing ptpd sync via ethernet. This is needed to timestamp consistently inside the DS-TT and NW-TT. Normally the clocks of gNB and UE will be synced via the 5GS RRC but this is not implemented yet."
+# This can also be tested manually by adding --foreground
+sudo ptpd -c $SCRIPTPATH/../config/eth_ptpd_client.config
+
+echo "Launching software ptp client"
+docker compose up -d ptp-client
+
+echo "Setting up routing for PTP packets"
 # For network graph see docs/structure.drawio
 # This can only run after the data session has been established by the 5gs (oaitun_ue1 created)
 
